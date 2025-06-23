5a92672500a81381196b6e1d8dc89152341a4621
savely3 <152527998+savely3@users.noreply.github.com>
2025-06-23 13:16:15 +0300

    Update Lab3_task2.py

--- модуль 1 лаба 3/Lab3_task2.py
+++ модуль 1 лаба 3/Lab3_task2.py
@@ -1,4 +1,3 @@
-# TODO Напишите функцию find_common_participants
 def find_common_participants(a, b, c=","):
     out = []
     a = a.split(c)
@@ -14,7 +13,5 @@ def find_common_participants(a, b, c=","):
 
 participants_first_group = "Иванов|Петров|Сидоров"
 participants_second_group = "Петров|Сидоров|Смирнов"
-
-# TODO Провеьте работу функции с разделителем отличным от запятой
 
 print(find_common_participants(participants_first_group, participants_second_group, "|"))

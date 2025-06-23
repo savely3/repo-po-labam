e312f685f44783ff35798f7da04d9cbc0deae783
savely3 <152527998+savely3@users.noreply.github.com>
2025-06-23 13:16:30 +0300

    Update Lab3_task2.py

--- модуль 1 лаба 3/Lab3_task2.py
+++ модуль 1 лаба 3/Lab3_task2.py
@@ -15,3 +15,4 @@ participants_first_group = "Иванов|Петров|Сидоров"
 participants_second_group = "Петров|Сидоров|Смирнов"
 
 print(find_common_participants(participants_first_group, participants_second_group, "|"))
+
